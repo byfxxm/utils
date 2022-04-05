e05b3fdf708f5387bf9bbf42095181ba910d5b99
byfxxm <byfxxm@outlook.com>
2022-04-05 15:29:52 +0800

    22

--- utils/utils/test.cpp
+++ utils/utils/test.cpp
@@ -8,11 +8,9 @@ int main()
 	for (int i = 0; i < a[0]; ++i)
 		for (int j = 0; j < a[1]; ++j)
 			for (int k = 0; k < a[2]; ++k)
-				a(i, j, k) = (char)i * j * k;
+				a(i, j, k) = (type)i * j * k;
 
-	auto b = a(4, 7, 8);
-	auto c = a[1];
-
+	assert(8 == a[1]);
 	assert(&a(2, 3, 4) - &a(1, 2, 3) == 82);
 	assert(&a(6, 7, 8) - &a(0, 0, 0) == 7 * 8 * 9 - 1);
 	assert((char*)&a(6, 7, 8) - (char*)&a(0, 0, 0) == (7 * 8 * 9 - 1) * sizeof(type));
