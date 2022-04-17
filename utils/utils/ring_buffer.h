abd9a218812399fe0d7b05c8363d5ebd22be170b
byfxxm <byfxxm@outlook.com>
2022-04-18 07:09:42 +0800

    22

---
+++ utils/utils/ring_buffer.h
@@ -0,0 +1,40 @@
+#pragma once
+
+template<typename T, int N>
+class RingBuffer {
+public:
+	void Reset() {
+		read_index_ = write_index_;
+	}
+
+	bool IsEmpty() {
+		return read_index_ == write_index_;
+	}
+
+	bool IsFull() {
+		return (write_index_ + 1) % N == read_index_;
+	}
+
+	bool Write(T t) {
+		if (IsFull())
+			return false;
+
+		data_[write_index_] = t;
+		write_index_ = (write_index_ + 1) % N;
+		return true;
+	}
+
+	bool Read(T& t) {
+		if (IsEmpty())
+			return false;
+
+		t = data_[read_index_];
+		read_index_ = (read_index_ + 1) % N;
+		return true;
+	}
+
+private:
+	volatile int read_index_{ 0 };
+	volatile int write_index_{ 0 };
+	T data_[N];
+};
