88fc72878a3c14f9e227c3a5fa0b6b0fb53d514d
byfxxm <byfxxm@outlook.com>
2022-04-18 18:40:31 +0800

    22

--- utils/utils/variable_buffer.h
+++ utils/utils/variable_buffer.h
@@ -4,7 +4,7 @@ template<size_t N>
 class VariableBuffer {
 public:
 	struct Head {
-		size_t data_size;
+		size_t data_count;
 	};
 
 public:
@@ -28,25 +28,14 @@ public:
 		return (read_index_ + N - write_index_ - 1) % N;
 	}
 
-	//bool PeekSize(size_t& size) const {
-	//	if (IsEmpty())
-	//		return false;
-
-	//	if (N - read_index_ < count) {
-	//		return;
-	//	}
-
-	//	buffer_[]
-	//}
-
-	bool Write(const char* data, size_t size) {
-		if (Free() < size + sizeof(Head))
+	bool Write(const char* buffer, size_t count) {
+		if (Free() < count + sizeof(Head))
 			return false;
 
-		Head head{ size };
+		Head head{ count };
 		auto index = write_index_;
 		Write(index, (char*)&head, sizeof(head));
-		Write(index, data, size);
+		Write(index, buffer, count);
 		write_index_ = index;
 		assert(write_index_ < N);
 		return true;
@@ -57,57 +46,51 @@ public:
 		return Write((char*)arr, sizeof(arr));
 	}
 
-	bool Read(char* data, size_t& size) {
+	bool Read(char* buffer, size_t& count) {
 		if (IsEmpty())
 			return false;
 
 		Head head{ 0 };
 		auto index = read_index_;
 		Read(index, (char*)&head, sizeof(head));
-		Read(index, data, head.data_size);
-		size = head.data_size;
+		Read(index, buffer, head.data_count);
+		count = head.data_count;
 		read_index_ = index;
 		assert(read_index_ < N);
 		return true;
 	}
 
-	//template<typename T, size_t N>
-	//bool Read(T(&arr)[N], size_t& size) {
-	//	decltype(buffer_) buf{ 0 };
-	//	return Read((char*)arr, sizeof(arr));
-	//}
-
 private:
-	void Write(size_t& index, const char* data, size_t count) {
+	void Write(size_t& index, const char* buffer, size_t count) {
 		if (N - index < count) {
 			auto part1 = N - index;
 			auto part2 = count - part1;
-			memcpy(&buffer_[index], data, part1);
-			memcpy(buffer_, &data[part1], part2);
+			memcpy(&data_[index], buffer, part1);
+			memcpy(data_, &buffer[part1], part2);
 			index = part2;
 			return;
 		}
 
-		memcpy(&buffer_[index], data, count);
+		memcpy(&data_[index], buffer, count);
 		index = (index + count) % N;
 	}
 
-	void Read(size_t& index, char* data, size_t count) {
+	void Read(size_t& index, char* buffer, size_t count) {
 		if (N - index < count) {
 			auto part1 = N - index;
 			auto part2 = count - part1;
-			memcpy(data, &buffer_[index], part1);
-			memcpy(&data[part1], buffer_, part2);
+			memcpy(buffer, &data_[index], part1);
+			memcpy(&buffer[part1], data_, part2);
 			index = part2;
 			return;
 		}
 
-		memcpy(data, &buffer_[index], count);
+		memcpy(buffer, &data_[index], count);
 		index = (index + count) % N;
 	}
 
 private:
 	volatile size_t read_index_{ 0 };
 	volatile size_t write_index_{ 0 };
-	char buffer_[N]{};
+	char data_[N]{};
 };
