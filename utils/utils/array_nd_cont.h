58165426fe04ab5cc731dde72eca5bbbacd07684
byfxxm <byfxxm@outlook.com>
2022-08-15 19:11:17 +0800

    22

--- utils/utils/array_nd_cont.h
+++ utils/utils/array_nd_cont.h
@@ -10,10 +10,42 @@
 *		char c = arr(1, 2, 3); // 随机访问数组，参数个数等于维数时，返回引用
 *		char* d = arr(5); // 参数个数小于维数时，返回指针
 *		size_t e = arr[0]; // 获取第一维度
-*		arr.Reset(0xff); // 内存初始化，默认每个字节初始化为 0
+*		arr.Memset(0xff); // 内存初始化，默认每个字节初始化为 0
 */
 template <typename T, size_t N, typename = std::enable_if_t<(N > 0)>>
-class ArrayNd {
+class ArrayNd final {
+private:
+	template <size_t N>
+	class BassPtr {
+	private:
+		using _Base = BassPtr<N - 1>;
+
+	public:
+		BassPtr(T* p, size_t(&d)[N]) : ptr_(p), dim_(d) {}
+
+		_Base& operator[](size_t idx) {
+			return _Base(&T[dim_[0]], static_cast<size_t[N - 1]>(dim_[1]));
+		}
+
+	private:
+		T ptr_{ nullptr };
+		size_t dim_[N]{};
+	};
+
+	template <>
+	class BassPtr<1> {
+	public:
+		BassPtr(T* p, size_t(&d)[1]) : ptr_(p), dim_(d[0]) {}
+
+		T& operator[](size_t idx) {
+			return T[dim_];
+		}
+
+	private:
+		T ptr_{ nullptr };
+		size_t dim_{ 0 };
+	};
+
 public:
 	template <typename... Ts, typename = std::enable_if_t<sizeof...(Ts) == N>>
 	ArrayNd(Ts&&... ts) {
@@ -26,7 +58,7 @@ public:
 		}
 
 		ele_ = new T[ele_cnt_];
-		Reset();
+		Memset();
 	}
 
 	ArrayNd(const ArrayNd& arr) {
@@ -46,7 +78,7 @@ public:
 		delete[] ele_;
 	}
 
-	void Reset(unsigned char val = 0) {
+	void Memset(unsigned char val = 0) {
 		assert(ele_);
 		memset(ele_, val, ele_cnt_ * sizeof(T));
 	}
@@ -63,10 +95,14 @@ public:
 		return Index(0, ele_, std::forward<Ts>(ts)...);
 	}
 
-	size_t operator[](size_t idx) const {
-		assert(idx >= 0 && idx < N);
-		assert(ele_);
-		return dim_[idx];
+	//size_t operator[](size_t idx) const {
+	//	assert(idx >= 0 && idx < N);
+	//	assert(ele_);
+	//	return dim_[idx];
+	//}
+
+	BassPtr<N - 1>& operator[](size_t idx) {
+		return BassPtr<N - 1>(ele_, static_cast<size_t[N - 1]>(dim_[1]));
 	}
 
 	ArrayNd& operator=(const ArrayNd& arr) {
--- utils/utils/test.cpp
+++ utils/utils/test.cpp
@@ -14,62 +14,7 @@
 #include "asm.h"
 
 void TestArrayNdCont() {
-	class A {
-	public:
-		A() {}
-		~A() {}
-	};
-
-	using type = __int64;
-	ArrayNd<type, 3> a(7, 8, 9);
-	type a1[7][8][9]{};
-
-	for (size_t i = 0; i < a[0]; ++i)
-		for (size_t j = 0; j < a[1]; ++j)
-			for (size_t k = 0; k < a[2]; ++k) {
-				a(i, j, k) = (type)i * j * k;
-				a1[i][j][k] = (type)i * j * k;
-			}
-
-	for (auto it : a) {
-		std::cout << it << std::endl;
-	}
-
-	assert(memcmp(a(), a1, sizeof(a1)) == 0);
-	assert(8 == a[1]);
-	assert(a(2, 3, 4) == 24);
-	assert(&a(2, 3, 4) - &a(1, 2, 3) == 82);
-	assert(&a(6, 7, 8) - &a(0, 0, 0) == 7 * 8 * 9 - 1);
-	assert((char*)&a(6, 7, 8) - (char*)&a(0, 0, 0) == (7 * 8 * 9 - 1) * sizeof(type));
-
-	ArrayNd<type, 5> b(7, 8, 9, 10, 11);
-	assert(&b(6, 7, 8, 9, 10) - &b(0, 0, 0, 0, 0) == 7 * 8 * 9 * 10 * 11 - 1);
-	assert(&b(2, 3, 4, 5, 6) - &b(2, 1, 2, 1, 2) == 2 * 9 * 10 * 11 + 2 * 10 * 11 + 4 * 11 + 4);
-
-	ArrayNd<type, 3> c(a);
-	assert(&c(6, 7, 8) - &c(0, 0, 0) == 7 * 8 * 9 - 1);
-
-	ArrayNd<type, 3> d(std::move(a));
-	assert(&d(6, 7, 8) - &d(0, 0, 0) == 7 * 8 * 9 - 1);
-	assert(d(1, 2) - d(0, 0) == 8 * 9 + 2 * 9);
-
-	ArrayNd<type, 5> e(1, 2, 3, 4, 5);
-	e = b;
-	assert(&e(6, 7, 8, 9, 10) - &e(0, 0, 0, 0, 0) == 7 * 8 * 9 * 10 * 11 - 1);
-	assert(&e(2, 3, 4, 5, 6) - &e(2, 1, 2, 1, 2) == 2 * 9 * 10 * 11 + 2 * 10 * 11 + 4 * 11 + 4);
-
-	std::vector<ArrayNd<type, 3>> v;
-	v.push_back(d);
-	v.push_back(d);
-	assert(v[0]() != v[1]());
-	assert(v[0](1, 2, 3) == 6);
-	v[0].Reset(0xff);
-	assert(v[0](1, 2, 3) == -1);
-
-	ArrayNd<A, 2> f(1, 2);
-	ArrayNd<A, 2> f1(f);
-	ArrayNd<A, 2> f2(3, 4);
-	f = f2;
+	ArrayNd<size_t, 3> a1(7, 8, 9);
 }
 
 void TestArrayNd() {
