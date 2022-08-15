8b257c3dfcaaf3539646c56112ee166841646196
byfxxm <byfxxm@outlook.com>
2022-08-15 20:32:45 +0800

    22

--- utils/utils/array_nd_cont.h
+++ utils/utils/array_nd_cont.h
@@ -18,22 +18,22 @@ private:
 	template <size_t N>
 	class BasePtr {
 	public:
-		BasePtr(T* p, const size_t* d, const size_t* f) : ptr_(p), dim_(d), factor_(f) {}
+		BasePtr(T* p, const size_t* d, const size_t* f) : ptr_(p), dims_(d), factors_(f) {}
 
 		BasePtr<N - 1> operator[](size_t idx) {
-			return BasePtr<N - 1>(&ptr_[dim_[0]], dim_ + 1, factor_ + 1);
+			return BasePtr<N - 1>(&ptr_[dims_[0]], dims_ + 1, factors_ + 1);
 		}
 
 	private:
 		T* ptr_{ nullptr };
-		const size_t* dim_{ nullptr };
-		const size_t* factor_{ nullptr };
+		const size_t* dims_{ nullptr };
+		const size_t* factors_{ nullptr };
 	};
 
 	template <>
 	class BasePtr<1> {
 	public:
-		BasePtr(T* p, const size_t* d, const size_t* f) : ptr_(p), dim_(d), factor_(f) {}
+		BasePtr(T* p, const size_t* d, const size_t* f) : ptr_(p), dims_(d), factors_(f) {}
 
 		T& operator[](size_t idx) {
 			return ptr_[idx];
@@ -41,114 +41,88 @@ private:
 
 	private:
 		T* ptr_{ nullptr };
-		const size_t* dim_{ nullptr };
-		const size_t* factor_{ nullptr };
+		const size_t* dims_{ nullptr };
+		const size_t* factors_{ nullptr };
 	};
 
 public:
-	template <typename... Ts, typename = std::enable_if_t<sizeof...(Ts) == N>>
-	ArrayNd(Ts&&... ts) {
-		EmplaceDim(0, std::forward<Ts>(ts)...);
-		for (auto i = 0; i < N; ++i) {
-			ele_cnt_ *= dim_[i];
-			factor_[i] = 1;
-			for (auto j = i + 1; j < N; ++j)
-				factor_[i] *= dim_[j];
+	template <typename... Args, typename = std::enable_if_t<sizeof...(Args) == N>>
+	ArrayNd(Args&&... args) {
+		SetDims(0, std::forward<Args>(args)...);
+		for (size_t i = 0; i < N; ++i) {
+			len_ *= dims_[i];
+			factors_[i] = 1;
+			for (size_t j = i + 1; j < N; ++j)
+				factors_[i] *= dims_[j];
 		}
 
-		ele_ = new T[ele_cnt_];
+		mem_ = new T[len_];
 		Memset(0);
 	}
 
 	ArrayNd(const ArrayNd& arr) {
-		assert(arr.ele_);
+		assert(arr.mem_);
 		memcpy(this, &arr, sizeof(arr));
-		ele_ = new T[ele_cnt_];
-		CopyEle(arr.ele_);
+		mem_ = new T[len_];
+		CopyMem(arr.mem_);
 	}
 
 	ArrayNd(ArrayNd&& arr) noexcept {
-		assert(arr.ele_);
+		assert(arr.mem_);
 		memcpy(this, &arr, sizeof(arr));
-		arr.ele_ = nullptr;
+		arr.mem_ = nullptr;
 	}
 
 	~ArrayNd() {
-		delete[] ele_;
+		delete[] mem_;
 	}
 
 	template <typename T>
 	void Memset(T&& val) {
-		assert(ele_);
-		for (size_t i = 0; i < ele_cnt_; ++i) {
-			ele_[i] = std::forward<T>(val);
+		assert(mem_);
+		for (size_t i = 0; i < len_; ++i) {
+			mem_[i] = std::forward<T>(val);
 		}
 	}
 
-	template <typename... Ts, typename = std::enable_if_t<sizeof...(Ts) == N>>
-	T& operator()(Ts&&... ts) const {
-		assert(ele_);
-		return *Index(0, ele_, std::forward<Ts>(ts)...);
-	}
-
-	template <typename... Ts, typename = std::enable_if_t<sizeof...(Ts) < N>>
-	T* operator()(Ts&&... ts) const {
-		assert(ele_);
-		return Index(0, ele_, std::forward<Ts>(ts)...);
-	}
-
 	BasePtr<N - 1> operator[](size_t idx) {
-		return BasePtr<N>(ele_, dim_, factor_)[idx];
+		return BasePtr<N>(mem_, dims_, factors_)[idx];
 	}
 
 	ArrayNd& operator=(const ArrayNd& arr) {
 		if (this == &arr)
 			return *this;
 
-		assert(arr.ele_);
-		ele_cnt_ = arr.ele_cnt_;
-		memcpy(dim_, arr.dim_, sizeof(dim_));
-		memcpy(factor_, arr.factor_, sizeof(factor_));
+		assert(arr.mem_);
+		len_ = arr.len_;
+		memcpy(dims_, arr.dims_, sizeof(dims_));
+		memcpy(factors_, arr.factors_, sizeof(factors_));
 
-		delete[] ele_;
-		ele_ = new T[ele_cnt_];
-		CopyEle(arr.ele_);
+		delete[] mem_;
+		mem_ = new T[len_];
+		CopyMem(arr.mem_);
 		return *this;
 	}
 
-	T* begin() {
-		return ele_;
-	}
-
-	T* end() {
-		return ele_ + ele_cnt_;
-	}
-
 private:
-	template <typename T1, typename... Ts, typename = std::enable_if_t<std::is_integral_v<std::remove_reference_t<T1>>>>
-	void EmplaceDim(size_t idx, T1&& t1, Ts&&... ts) {
-		dim_[idx] = t1;
-		if constexpr (sizeof...(ts) > 0)
-			EmplaceDim(idx + 1, std::forward<Ts>(ts)...);
+	template <typename... Args>
+	void SetDims(size_t idx, size_t first, Args... args) {
+		dims_[idx] = first;
+		if constexpr (sizeof...(args) > 0)
+			SetDims(idx + 1, args...);
 	}
 
-	template <typename T1, typename... Ts, typename = std::enable_if_t<std::is_integral_v<std::remove_reference_t<T1>>>>
-	T* Index(size_t idx, T* p, T1&& t1, Ts&&... ts) const {
-		assert((size_t)t1 < dim_[idx]);
-		return Index(idx + 1, &p[factor_[idx] * t1], std::forward<Ts>(ts)...);
-	}
-
-	void CopyEle(T* p) {
+	void CopyMem(T* p) {
 		if constexpr (std::is_trivial_v<T>)
-			memcpy(ele_, p, ele_cnt_ * sizeof(T));
+			memcpy(mem_, p, len_ * sizeof(T));
 		else
-			for (size_t i = 0; i < ele_cnt_; ++i)
-				ele_[i] = p[i];
+			for (size_t i = 0; i < len_; ++i)
+				mem_[i] = p[i];
 	}
 
 private:
-	T* ele_{ nullptr };
-	size_t ele_cnt_{ 1 };
-	size_t dim_[N]{};
-	size_t factor_[N]{};
+	T* mem_{ nullptr };
+	size_t len_{ 1 };
+	size_t dims_[N]{};
+	size_t factors_[N]{};
 };
