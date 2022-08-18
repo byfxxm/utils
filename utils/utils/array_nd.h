fd3fda35d59c8038128b4c652716b7483ab4da66
byfxxm <byfxxm@outlook.com>
2022-08-18 20:52:59 +0800

    22

--- utils/utils/array_nd.h
+++ utils/utils/array_nd.h
@@ -17,45 +17,45 @@ namespace array_nd {
 			template <size_t N>
 			class ViewPtr final {
 			public:
-				ViewPtr(T* p, const size_t* d, const size_t* f) : ptr_(p), dimensions_(d), factors_(f) {
+				ViewPtr(T* p, const size_t* d, const size_t* f) : ptr_(p), dims_(d), factors_(f) {
 					assert(ptr_);
 				}
 
 				ViewPtr<N - 1> operator[](size_t idx) const&& {
-					assert(idx >= 0 && idx < dimensions_[0]);
-					return ViewPtr<N - 1>(ptr_ + idx * factors_[0], dimensions_ + 1, factors_ + 1);
+					assert(idx >= 0 && idx < dims_[0]);
+					return ViewPtr<N - 1>(ptr_ + idx * factors_[0], dims_ + 1, factors_ + 1);
 				}
 
 			private:
 				T* ptr_{ nullptr };
-				const size_t* dimensions_{ nullptr };
+				const size_t* dims_{ nullptr };
 				const size_t* factors_{ nullptr };
 			};
 
 			template <>
 			class ViewPtr<1> {
 			public:
-				ViewPtr(T* p, const size_t* d, const size_t*) : ptr_(p), dimensions_(d) {}
+				ViewPtr(T* p, const size_t* d, const size_t*) : ptr_(p), dims_(d) {}
 
 				T& operator[](size_t idx) const&& {
-					assert(idx >= 0 && idx < dimensions_[0]);
+					assert(idx >= 0 && idx < dims_[0]);
 					return ptr_[idx];
 				}
 
 			private:
 				T* ptr_{ nullptr };
-				const size_t* dimensions_{ nullptr };
+				const size_t* dims_{ nullptr };
 			};
 
 		public:
 			template <class... Args> requires (sizeof...(Args) == N)
-			ArrayNd(Args&&... args) : count_((... * args)), dimensions_{ static_cast<size_t>(args)... } {
-				element_ = std::make_shared<T[]>(count_);
+			ArrayNd(Args&&... args) : count_((... * args)), dims_{ static_cast<size_t>(args)... } {
+				elems_ = std::make_shared<T[]>(count_);
 				Memset(0);
 				for (size_t i = 0; i < N; ++i) {
 					factors_[i] = 1;
 					for (size_t j = i + 1; j < N; ++j)
-						factors_[i] *= dimensions_[j];
+						factors_[i] *= dims_[j];
 				}
 			}
 
@@ -65,19 +65,19 @@ namespace array_nd {
 			ArrayNd& operator=(ArrayNd&&) noexcept = default;
 
 			decltype(auto) operator[](size_t idx) {
-				return ViewPtr<N>(element_.get(), &dimensions_.front(), &factors_.front())[idx];
+				return ViewPtr<N>(elems_.get(), &dims_.front(), &factors_.front())[idx];
 			}
 
 			void Memset(T val) {
-				assert(element_);
+				assert(elems_);
 				for (size_t i = 0; i < count_; ++i)
-					element_[i] = val;
+					elems_[i] = val;
 			}
 
 		private:
-			std::shared_ptr<T[]> element_;
+			std::shared_ptr<T[]> elems_;
 			size_t count_{ 0 };
-			std::array<size_t, N> dimensions_;
+			std::array<size_t, N> dims_;
 			std::array<size_t, N> factors_;
 	};
 }
