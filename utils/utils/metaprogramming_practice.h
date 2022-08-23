58de1366ceaf6588c0e27a88db17d720e11707c6
byfxxm <byfxxm@outlook.com>
2022-08-23 20:26:59 +0800

    22

--- utils/utils/metaprogramming_practice.h
+++ utils/utils/metaprogramming_practice.h
@@ -4,32 +4,36 @@ namespace Meta {
 	/// <summary>
 	/// 判断是否有子类型模板，利用SFINAE特性实现
 	/// </summary>
-	template <class T>
-	constexpr bool _HasType(typename T::type*){
-		return true;
-	}
+	namespace {
+		template <class T>
+		constexpr bool _HasType(typename T::type*) {
+			return true;
+		}
 
-	template <class>
-	constexpr bool _HasType(...) {
-		return false;
-	}
+		template <class>
+		constexpr bool _HasType(...) {
+			return false;
+		}
 
-	template <class T>
-	static constexpr bool HasType = _HasType<T>(nullptr);
+		template <class T>
+		constexpr bool HasType = _HasType<T>(nullptr);
+	}
 
 	/// <summary>
 	/// 判断是否是自定义类型
 	/// </summary>
-	template <class>
-	constexpr bool _IsCustomeClass(...) {
-		return false;
-	}
+	namespace {
+		template <class>
+		constexpr bool _IsCustomeClass(...) {
+			return false;
+		}
 
-	template <class T>
-	constexpr bool _IsCustomeClass(int T::*) {
-		return true;
-	}
+		template <class T>
+		constexpr bool _IsCustomeClass(int T::*) {
+			return true;
+		}
 
-	template <class T>
-	static constexpr bool IsCustomeClass = _IsCustomeClass<T>(nullptr);
+		template <class T>
+		constexpr bool IsCustomeClass = _IsCustomeClass<T>(nullptr);
+	}
 }
