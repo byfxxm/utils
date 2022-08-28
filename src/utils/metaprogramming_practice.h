529a3abe07c05937948bdecbc93f15e0b6c32ac2
byfxxm <byfxxm@outlook.com>
2022-08-28 20:57:50 +0800

    22

--- src/utils/metaprogramming_practice.h
+++ src/utils/metaprogramming_practice.h
@@ -32,4 +32,29 @@ namespace Meta {
 
 	template <class T>
 	inline constexpr bool IsCustomeClass = _IsCustomeClass<T>(nullptr);
+
+	/// <summary>
+	/// N->(0, ... , N - 1)
+	/// </summary>
+	/// <typeparam name="...N"></typeparam>
+	template <size_t... N>
+	struct Seq {
+		using type = Seq;
+	};
+
+	template <class>
+	struct Upgrade;
+
+	template <size_t... Ns>
+	struct Upgrade<Seq<Ns...>> {
+		using type = Seq<0, (Ns + 1)...>;
+	};
+
+	template <size_t N>
+	struct Make : Upgrade<typename Make<N - 1>::type>::type {
+	};
+
+	template <>
+	struct Make<0> : Seq<> {
+	};
 }
