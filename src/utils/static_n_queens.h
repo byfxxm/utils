aded2ac87fd1f4c791e0c7daea3613099bb214a1
byfxxm <byfxxm@outlook.com>
2022-09-20 06:14:52 +0800

    22

--- src/utils/static_n_queens.h
+++ src/utils/static_n_queens.h
@@ -2,11 +2,11 @@
 
 namespace byfxxm {
 	struct Queen {
-		constexpr bool InRange(const Queen& queen) const {
+		bool InRange(const Queen& queen) const {
 			return x == queen.y || y == queen.y || abs(x, queen.x) == abs(y, queen.y);
 		}
 
-		constexpr size_t abs(size_t a, size_t b) const {
+		size_t abs(size_t a, size_t b) const {
 			return a > b ? a - b : b - a;
 		}
 
@@ -14,9 +14,19 @@ namespace byfxxm {
 		size_t y = 0;
 	};
 
-	template <Queen... Qs>
+	template <Queen... Rest>
 	struct QueensArray {
 
+	};
+
+	template <size_t N>
+	struct Generate {
+		using type = typename Generate<N - 1>::type;
+	};
+
+	template <>
+	struct Generate<0> {
+		//using type = Queen{ .x = 0, .y = 0 };
 	};
 
 	template <class... QArrays>
