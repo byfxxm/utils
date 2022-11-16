4e95ac2e796f5b3f75a5c2f00e57c85250e425f8
byfxxm <byfxxm@outlook.com>
2022-11-16 23:08:40 +0800

    22

--- src/utils/array_nd.h
+++ src/utils/array_nd.h
@@ -59,7 +59,7 @@ namespace byfxxm {
 		template <ElementType... Args>
 			requires (sizeof...(Args) == Num)
 		ArrayNd(Args&&... args) : count_((... * args)), shapes_{ static_cast<size_t>(args)... } {
-			elems_ = std::make_unique<Ty[]>(count_);
+			elems_ = std::make_unique<Ty>(count_);
 			Memset(0);
 			InitializeFactors();
 		}
@@ -84,7 +84,7 @@ namespace byfxxm {
 				count_ *= it;
 			}
 
-			elems_ = std::make_unique<Ty[]>(count_);
+			elems_ = std::make_unique<Ty>(count_);
 			Memset(0);
 			InitializeFactors();
 			Assignment(list, 0, 0);
@@ -98,7 +98,7 @@ namespace byfxxm {
 			count_ = arr.count_;
 			shapes_ = arr.shapes_;
 			factors_ = arr.factors_;
-			elems_ = std::make_unique<Ty[]>(count_);
+			elems_ = std::make_unique<Ty>(count_);
 			memcpy(elems_.get(), arr.elems_.get(), count_ * sizeof(Ty));
 			return *this;
 		}
@@ -113,14 +113,14 @@ namespace byfxxm {
 		template <class Predicate>
 		void ForEach(Predicate&& pred) {
 			for (size_t i = 0; i < count_; ++i) {
-				elems_[i] = pred(elems_[i]);
+				elems_.get()[i] = pred(elems_.get()[i]);
 			}
 		}
 
 		void Memset(Ty val) {
 			assert(elems_);
 			for (size_t i = 0; i < count_; ++i) {
-				elems_[i] = val;
+				elems_.get()[i] = val;
 			}
 		}
 
@@ -161,7 +161,7 @@ namespace byfxxm {
 
 		void Assignment(std::initializer_list<Ty> list, size_t, size_t offset) {
 			for (auto it = list.begin(); it != list.end(); ++it) {
-				elems_[offset + (it - list.begin())] = *it;
+				elems_.get()[offset + (it - list.begin())] = *it;
 			}
 		}
 
@@ -174,7 +174,7 @@ namespace byfxxm {
 
 	private:
 		size_t count_{ 0 };
-		std::unique_ptr<Ty[]> elems_;
+		std::unique_ptr<Ty> elems_;
 		std::array<size_t, Num> shapes_;
 		std::array<size_t, Num> factors_;
 	};
